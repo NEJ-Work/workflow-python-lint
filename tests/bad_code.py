e7aec992c4e682e6a9906dbba8fda0500acd9507
NEJ-Work <nathanjanero@gmail.com>
2025-11-19 15:01:20 -0500

    fixed bad code

--- tests/bad_code.py
+++ tests/bad_code.py
@@ -1,7 +1,12 @@
-def poorly_formatted(x,y):
-    if x>y:return x
+# Fix the formatting in bad_code.py
+def poorly_formatted(x, y):
+    if x > y:
+        return x
     else:
-            return y
-def another_function( a, b ):
-    result=a+b
+        return y
+
+
+def another_function(a, b):
+    result = a + b
     return result
+
